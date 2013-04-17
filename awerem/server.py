3e6f870e91196f9ea29ca0e2f9bcaf1ea3dc9b48
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-04-17 19:06:57 +0200

    Separation of the configuration, the ui and the action of the modules

--- awerem/server.py
+++ awerem/server.py
@@ -2,30 +2,67 @@
 # -*- coding:utf8 -*-
 
 from twisted.web import xmlrpc, server
+from twisted.web.static import File
+from twisted.web.resource import Resource, NoResource
+import os.path
 from yapsy.PluginManager import PluginManagerSingleton
 from yapsy.PluginFileLocator import (PluginFileLocator,
                                      PluginFileAnalyzerWithInfoFile)
 from modules.aweremplugin import AweRemPlugin
 
 
-class ModulesManager(xmlrpc.XMLRPC):
+def init_pm():
+    pluginLocator = PluginFileLocator()
+    pluginLocator.setPluginPlaces(["modules"])
+    pluginLocator.appendAnalyzer(
+        PluginFileAnalyzerWithInfoFile("AweRemModules", extensions="arm"))
+    pm = PluginManagerSingleton.get()
+    pm.setPluginLocator(pluginLocator)
+    pm.setCategoriesFilter({"Default": AweRemPlugin})
+    pm.collectPlugins()
+    for plugin in pm.getAllPlugins():
+        pm.activatePluginByName(plugin.name)
+    return pm
 
+
+class ActionsManager(xmlrpc.XMLRPC):
+    """
+    Resource that executes all the actions provided by the plugins
+    """
     def __init__(self):
         xmlrpc.XMLRPC.__init__(self)
-        pluginLocator = PluginFileLocator()
-        pluginLocator.setPluginPlaces(["modules"])
-        pluginLocator.appendAnalyzer(
-            PluginFileAnalyzerWithInfoFile("AweRemModules", extensions="arm"))
         self.pm = PluginManagerSingleton.get()
-        self.pm.setPluginLocator(pluginLocator)
-        self.pm.setCategoriesFilter({"Default": AweRemPlugin})
-        self.pm.collectPlugins()
         for plugin in self.pm.getAllPlugins():
-            self.pm.activatePluginByName(plugin.name)
             self.putSubHandler(plugin.name, plugin.plugin_object.getHandler())
+
+
+class UIManager(Resource):
+    """
+    Resource that manage all the ui provided by the plugins
+    """
+
+    def __init__(self):
+        Resource.__init__(self)
+        self.pm = PluginManagerSingleton.get()
+
+    def getChild(self, name, request):
+        try:
+            plugin = self.pm.getPluginByName(name)
+        except:
+            return NoResource()
+        else:
+            return File(os.path.join(os.path.dirname(plugin.path), "ui.html"))
+
+
+class ConfigureManager(xmlrpc.XMLRPC):
+    pass
 
 if __name__ == '__main__':
     from twisted.internet import reactor
-    r = ModulesManager()
+    init_pm()
+    r = Resource()
+    r.putChild("action", ActionsManager())
+    r.putChild("ui", UIManager())
+    r.putChild("configure", ConfigureManager())
     reactor.listenTCP(34340, server.Site(r))
     reactor.run()
