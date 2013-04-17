3cfd260d4b3bb1c5eaab7313f9ceddd703dbd232
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-04-17 14:29:16 +0200

    Modification of redbutton for testing purpose

--- awerem/modules/redbutton/redbutton.py
+++ awerem/modules/redbutton/redbutton.py
@@ -1,8 +1,27 @@
 from modules.aweremplugin import AweRemPlugin
+from twisted.web import xmlrpc
+
+
+class RedButtonHandler(xmlrpc.XMLRPC):
+
+    def __init__(self, redbutton):
+        self.redbutton = redbutton
+
+    def xmlrpc_press(self):
+        """
+        Print "RedButton Triggered"
+        """
+        print "RedButton Triggered"
+        return True
 
 
 class RedButton(AweRemPlugin):
-    """Print "RedButton Triggered" when polled"""
+    """
+    Print "RedButton Triggered"
+    """
 
-    def do(self, args):
-        print("RedButton Triggered")
+    def activate(self):
+        self.handler = RedButtonHandler(self)
+
+    def getHandler(self):
+        return self.handler
