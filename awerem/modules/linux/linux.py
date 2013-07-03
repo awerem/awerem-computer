7f935b7a8ee17fdf310f9c46d1ac3f2be97b0484
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-07-03 19:00:52 +0200

    Addition of the setVolume method (not tested)

--- awerem/modules/linux/linux.py
+++ awerem/modules/linux/linux.py
@@ -2,6 +2,8 @@ from modules.aweremplugin import AweRemPlugin
 import subprocess
 from twisted.web import xmlrpc
 from twisted.internet import reactor
+import platform
+import re
 
 
 class LinuxHandler(xmlrpc.XMLRPC):
@@ -36,11 +38,29 @@ class LinuxRemote(AweRemPlugin):
     """
 
     def activate(self):
-        self.shutTimer = None
+        if platform.system().lower() == "linux":
+            self.shutTimer = None
+            self.priority = 2
+            self.isLinux = True
+        else:
+            self.priority = -1
         self.handler = LinuxHandler(self)
 
     def getHandler(self):
         return self.handler
+
+    def getInfo(self):
+        return {"title": self.getName(), "category": "contextual",
+                "priority": self.priority}
+
+    def getName(self):
+        if not self.isLinux:
+            return "Not linux"
+        else:
+            if platform.linux_distribution() != "":
+                return platform.linux_distribution()[0]
+            else:
+                return "Linux"
 
     def shutdown(self, s, m, h, d):
         shutTime = d * 24 * 3600 + h * 3600 + m * 60 + s
@@ -51,3 +71,35 @@ class LinuxRemote(AweRemPlugin):
         self.shutTimer = reactor.callLater(shutTime, lambda: subprocess.call(
             ["gnome-session-quit", "--power-off"]))
         return True
+
+    def updateVolume(self, volume):
+        volume = int(volume)
+        if volume > 100:
+            volume = 100
+        elif volume < 0:
+            volume = 0
+        succeeded_once = False
+        try:
+            sinks_list = subprocess.check_output(["pactl", "list", "short",
+                                                  "sinks"])
+        except subprocess.CalledProcessError:
+            print("pulseaudio is not used")
+        else:
+            for match in re.findIter(r"^(\d+).*?running", sinks_list,
+                                     re.MULTILINE):
+                sink = match.group(1)
+                volume_str = volume + "%"
+                try:
+                    subprocess.call(["pactl", "set-sink-volume", sink,
+                                     volume_str])
+                except:
+                    pass
+                else:
+                    succeeded_once = True
+            if succeeded_once:  # If it succeeded, plays a little sound
+                try:
+                    subprocess.call(["canberra-gtk-play",
+                                     "--id=audio-volume-change"])
+                except:
+                    pass
+        return self.getCurrentVolume()
