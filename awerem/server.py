ef9320f09b313b190b6feb0409ed07c53b4e0c0e
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-07-11 11:51:43 +0200

    xmlrpc allow none

--- awerem/server.py
+++ awerem/server.py
@@ -48,7 +48,7 @@ class ActionsManager(xmlrpc.XMLRPC):
     Resource that executes all the actions provided by the plugins
     """
     def __init__(self):
-        xmlrpc.XMLRPC.__init__(self)
+        xmlrpc.XMLRPC.__init__(self, allowNone=True)
         self.pm = PluginManagerSingleton.get()
         for plugin in self.pm.getAllPlugins():
             self.putSubHandler(plugin.name, plugin.plugin_object.getHandler())
