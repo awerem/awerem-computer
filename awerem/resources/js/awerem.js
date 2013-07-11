1e5d33f0c62a0e23b3e1c3b7dceae048f1f5b8b0
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-07-11 15:34:03 +0200

    Awerem.js helps for simpler args management

---
+++ awerem/resources/js/awerem.js
@@ -0,0 +1,37 @@
+var awerem = new function()
+{
+    this._id = 0;
+    this._callbacks = {};
+
+    this.sendAction = function(method)
+    {
+        var curid = this._id;
+        var args = Array.prototype.slice.call(arguments, 1);
+        for(var i = 0; i < args.length; i++)
+        {
+            _awerem.addArg(curid, args[i]);
+        }
+        _awerem.sendAction(curid, method);
+        this._id++;
+    };
+
+    this.sendActionAsync = function(method)
+    {
+        curid = this._id;
+        var args = Array.prototype.slice.call(arguments, 1);
+        if(typeof(args[-1]) === "function")
+            this._callbacks[id] = args.pop();
+        for(var i = 0; i < args.length; i++)
+        {
+            _awerem.addArg(curid, args[i]);
+        }
+        _awerem.sendActionAsync(curid, method)
+        this._id++;
+    };
+
+    this.triggerCallback = function(id)
+    {
+        if(typeof(callbacks[id]) === "function")
+            this._callbacks[id]();
+    }
+}();
