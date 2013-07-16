d2fd679e7ca551c0295ccead050532e3e4a4829b
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-07-16 19:48:40 +0200

    triggerCallback is working

--- awerem/resources/js/awerem.js
+++ awerem/resources/js/awerem.js
@@ -9,22 +9,25 @@ var awerem = new function()
         var args = Array.prototype.slice.call(arguments, 1);
         for(var i = 0; i < args.length; i++)
         {
-	    console.log(new String(args[i]));
-            _awerem.addString(curid, args[i]);
+            console.log(new String(args[i]));
+            this._addArg(curid, args[i]);
         }
-        _awerem.sendAction(curid, method);
         this._id++;
+        return _awerem.sendAction(curid, method);
     };
 
     this.sendActionAsync = function(method)
     {
         curid = this._id;
         var args = Array.prototype.slice.call(arguments, 1);
-        if(typeof(args[-1]) === "function")
-            this._callbacks[id] = args.pop();
+        if(typeof(args[args.length-1]) === "function")
+        {
+            console.log("here");
+            this._callbacks[curid] = args.pop();
+        }
         for(var i = 0; i < args.length; i++)
         {
-            _awerem.addArg(curid, args[i]);
+            this._addArg(curid, args[i]);
         }
         _awerem.sendActionAsync(curid, method)
         this._id++;
@@ -32,7 +35,26 @@ var awerem = new function()
 
     this.triggerCallback = function(id)
     {
-        if(typeof(callbacks[id]) === "function")
-            this._callbacks[id]();
-    }
+        if(typeof(this._callbacks[id]) === "function")
+        {
+            var args = Array.prototype.slice.call(arguments, 1);
+            this._callbacks[id].apply(this, args);
+        }
+    };
+
+    this._addArg = function(curid, arg)
+    {
+        type = typeof arg;
+        if(type === "string")
+            _awerem.addString(curid, arg)
+        else if (type === "boolean")
+            _awerem.addString(curid, arg)
+        else if (type === "number")
+        {
+            if (parseFloat(arg) == parseInt(arg, 10) && !isNaN(arg))
+                _awerem.addInt(arg)
+            else
+                _awerem.addFloat(arg)
+        }
+    };
 }();
