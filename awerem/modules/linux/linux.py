06cf3dcaa8fc01b993d8dfda3344033b392b2c49
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-07-18 00:49:21 +0200

    Linux remote now ported for the new plugin api

--- awerem/modules/linux/linux.py
+++ awerem/modules/linux/linux.py
@@ -1,74 +1,64 @@
-from modules.aweremplugin import AweRemPlugin
+from modules.aweremplugin import AweRemPlugin, AweRemHandler
 import subprocess
-from twisted.web import xmlrpc
 from twisted.internet import reactor
 import platform
 import re
-import json
 
 
-class LinuxHandler(xmlrpc.XMLRPC):
+class LinuxHandler(AweRemHandler):
     """
-    XMLRPC Handler for Linux Remote
+    AweRem Handler for Linux Remote
     """
     def __init__(self, linux):
-        xmlrpc.XMLRPC.__init__(self)
+        AweRemHandler.__init__(self)
         self.linux = linux
 
-    def xmlrpc_shutdown(self, jsonstr):
+    def out_shutdown(self, mode="power-off", seconds=0, minutes=0, hours=0):
         """
         Shutdown the computer at the given time
-        seconds - The number of seconds to wait
-        minutes - The number of minutes to wait
-        hours - the number of hours to wait
-        mode - The action to do: "power-off", "reboot"
+        mode -- The action to do: "power-off", "reboot"
+        seconds -- The number of seconds to wait
+        minutes -- The number of minutes to wait
+        hours -- the number of hours to wait
         """
-        if jsonstr:
-            data = json.loads(jsonstr)
-        else:
-            data = {}
         try:
-            s = int(data['seconds'])
+            s = int(seconds)
         except:
             s = 0
         try:
-            m = int(data['minutes'])
+            m = int(minutes)
         except:
             m = 0
         try:
-            h = int(data['hours'])
+            h = int(hours)
         except:
             h = 0
-        try:
-            mode = str(data['mode'])
-        except:
-            mode = "power-off"
         if mode != "power-off" and mode != "reboot":
             mode = "power-off"
         if s >= 0 and m >= 0 and h >= 0:
-            return json.dumps(self.linux.shutdown(mode, s, m, h))
+            return self.linux.shutdown(mode, s, m, h)
         else:
             raise ValueError("date must be positive")
 
-    def xmlrpc_updateVolume(self, jsonstr):
+    def out_updateVolume(self, volume, beep=False):
         """
         Update the volume of the system at the given percentage
         volume - the percentage of the volume to be set
         """
-        data = json.loads(jsonstr)
-        vol = int(data["volume"])
-        beep = bool(data["beep"])
+        print(repr(beep))
+        vol = int(volume)
+        beep = bool(beep)
         if vol > 100:
             vol = 100
         elif vol < 0:
             vol = 0
-        return json.dumps(self.linux.updateVolume(vol, beep))
+        return self.linux.updateVolume(vol, beep)
 
-    def xmlrpc_getCurrentVolume(self, unused):
+    def out_getCurrentVolume(self):
         return self.linux.getCurrentVolume()
 
-    def xmlrpc_beep(self, unused):
-        return json.dumps(self.linux.beep())
+    def out_beep(self):
+        return self.linux.beep()
 
 
 class LinuxRemote(AweRemPlugin):
@@ -122,13 +112,14 @@ class LinuxRemote(AweRemPlugin):
         except subprocess.CalledProcessError:
             print("pulseaudio is not used")
         else:
-            return re.search(r"\*.*?volume.*?(\d+)%", sinks_info,
-                             re.DOTALL).group(1)
+            return int(re.search(r"\*.*?volume.*?(\d+)%", sinks_info,
+                                 re.DOTALL).group(1))
 
     def updateVolume(self, volume, beep):
         succeeded_once = False
         try:
-            sinks_list = subprocess.check_output(["pacmd", "list-sinks"]).lower()
+            sinks_list = subprocess.check_output(
+                ["pacmd", "list-sinks"]).lower()
         except subprocess.CalledProcessError:
             print("pulseaudio is not used")
         else:
